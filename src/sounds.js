96b2ff6861124e99e51acf88d0ad19a6f4bab96e
Mirko Brevi <meowthcollectablebuyee@gmail.com>
2025-10-27 00:25:50 +0100

    fix: aggiunti file audio mancanti

--- src/sounds.js
+++ src/sounds.js
@@ -1,5 +1,5 @@
 export const SOUND_URLS = {
-  ding: '/sounds/ding.mp3',
-  boom: '/sounds/boom.mp3',
-  finish: '/sounds/finish.mp3'
+  ding: "/ding.mp3",
+  boom: "/boom.mp3",
+  finish: "/finish.mp3",
 };
