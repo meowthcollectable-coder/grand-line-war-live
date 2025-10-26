e9ae9dd0d1d531a8d119cdc508ef5e5933cb3543
meowthcollectable-coder <meowthcollectable@gmail.com>
2025-10-26 22:50:14 +0100

    Update sounds.js

--- src/sounds.js
+++ src/sounds.js
@@ -1,12 +1,6 @@
 export const SOUND_URLS = {
-  ambient: "",
-  ding: "/sounds/ding.mp3",
-  boom: "/sounds/boom.mp3",
-  finish: "/sounds/finish.mp3",
-  event_attack: "",
-  event_betrayal: "",
-  event_duel: "",
-  event_skypiea: "",
-  event_berries: "",
-  event_fruit: ""
+  ambient: '/sounds/ambient.mp3',
+  ding: '/sounds/ding.mp3',
+  boom: '/sounds/boom.mp3',
+  finish: '/sounds/finish.mp3'
 };
