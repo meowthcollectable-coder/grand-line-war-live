38cd88550a355cd6d476e2cd43d61521c39049d4
Mirko Brevi <meowthcollectablebuyee@gmail.com>
2025-10-27 19:37:41 +0100

    Fix compatibilità iPad iOS 12 (build ES2015)

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: "dist",
+    target: ["es2015"], // 👈 compatibile anche con iPad iOS 12
   },
   server: {
     port: 5173,
-    host: true
-  }
-})
+    host: true,
+  },
+});
